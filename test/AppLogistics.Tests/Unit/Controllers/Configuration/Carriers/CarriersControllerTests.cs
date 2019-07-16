5953ff659b11d43da2afef6e0b7655bbbadfd346
Andres Forero <forero08@gmail.com>
2019-07-15 22:24:56 -0500

    FIx carriers unit test. Add money format to some views

--- test/AppLogistics.Tests/Unit/Controllers/Configuration/Carriers/CarriersControllerTests.cs
+++ test/AppLogistics.Tests/Unit/Controllers/Configuration/Carriers/CarriersControllerTests.cs
@@ -22,6 +22,7 @@ namespace AppLogistics.Controllers.Configuration.Tests
         public CarriersControllerTests()
         {
             validator = Substitute.For<ICarrierValidator>();
+            validator.CanDelete(Arg.Any<int>()).Returns(true);
             service = Substitute.For<ICarrierService>();
 
             carrier = ObjectsFactory.CreateCarrierView();
